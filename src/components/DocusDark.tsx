012e30f026161857b3b82020eef294f74d7b04b5
ridemountainpig <92412722+ridemountainpig@users.noreply.github.com>
2025-09-15 00:31:00 +0000

    chore: update svgl component

---
+++ src/components/DocusDark.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const DocusDark = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 33 32"
+    width="100%"
+    height="100%"
+    preserveAspectRatio="xMidYMid meet"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M16.075 0C7.211 0 0 7.186 0 16.05v14.335C0 31.277.723 32 1.615 32H16.1c8.837 0 16-7.163 16-16S24.912 0 16.075 0m8.727 19.402a3.431 3.431 0 1 0 0-6.862 3.431 3.431 0 0 0 0 6.862m-10.184 0a3.431 3.431 0 1 0 0-6.862 3.431 3.431 0 0 0 0 6.862"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default DocusDark;
